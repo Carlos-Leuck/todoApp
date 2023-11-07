4594bfccd6503fd513e40ea62f72cb7272cd440e
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-11-07 13:52:28 -0300

    items são lidos do localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent {
         ]),
       ],
     });
+    this.load();
   }
 
   add() {
@@ -42,18 +43,26 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
   save() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data!);
   }
 }
